1bf1a415d893806fa64925a5ae01f13683e1e7ce
Jimmy Bamuteeze <jimmy.bamuteeze@unox.com>
2025-06-04 09:16:13 +0200

    Changes 04.06.2025

--- webapp/controller/Overview.controller.js
+++ webapp/controller/Overview.controller.js
@@ -1,14 +1,31 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/syncStyleClass"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, syncStyleClass) {
         "use strict";
 
         return Controller.extend("sap.ui5training.controller.Overview", {
+onSave: function () {
+  if (!this.pDialog) {
+    this.pDialog = this.loadFragment({
+      name: "sap.ui5training.view.Dialog"
+    }).then(function (oDialog) {
+      syncStyleClass(this.getOwnerComponent().getContentDensityClass(), this.getView(), oDialog);
+      return oDialog;
+    }.bind(this));
+  }
+  this.pDialog.then(function (oDialog) {
+    oDialog.open();
+  });
+},
 
+onCloseDialog: function () {
+  this.byId("dialog").close();
+}
 
         });
     });
